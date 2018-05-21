974f1d0ff1767a1aad9728285e6f6f2ceb55943e
Austin Nash <austinnash@Austins-MacBook-Pro.local>
2018-05-21 12:02:10 +1000

    path change aws-4

--- app/models/mail_item.rb
+++ app/models/mail_item.rb
@@ -8,6 +8,7 @@ class MailItem < ApplicationRecord
     has_attached_file :image,
                       :path => "/:basename.:extension"
                       # :AWS_REGION => 'us-east-2'
+                      :s3_domain_url => "******.s3.us-east-2.amazonaws.com",
     validates_attachment :image, :content_type => {:content_type => %w(image/jpeg image/jpg image/png application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document application/vnd.openxmlformats-officedocument.spreadsheetml.sheet)}
 
     has_attached_file :data
