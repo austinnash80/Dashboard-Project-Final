521676069294e00697b7584b16d294848dc91dcb
Austin Nash <austinnash@Austins-MacBook-Pro.local>
2018-05-21 12:33:51 +1000

    path change excel file

--- app/models/mail_item.rb
+++ app/models/mail_item.rb
@@ -8,12 +8,11 @@ class MailItem < ApplicationRecord
     has_attached_file :image,
                       :url =>':s3_domain_url',
                       :path => "/:basename.:extension"
-                      # :AWS_REGION => 'us-east-2'
-                      # :s3_domain_url => "******.s3.us-east-2.amazonaws.com"
-
     validates_attachment :image, :content_type => {:content_type => %w(image/jpeg image/jpg image/png application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document application/vnd.openxmlformats-officedocument.spreadsheetml.sheet)}
 
     has_attached_file :data
+                      :url =>':s3_domain_url',
+                      :path => "/:basename.:extension"
     validates_attachment :data, :content_type => {:content_type => %w(image/jpeg image/jpg image/png application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document application/vnd.openxmlformats-officedocument.spreadsheetml.sheet)}
 
 
