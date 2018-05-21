eea078026a73b81eb9ebd64524602f77e339786f
Austin Nash <austinnash@Austins-MacBook-Pro.local>
2018-05-21 11:51:10 +1000

    path change aws-1

--- app/models/mail_item.rb
+++ app/models/mail_item.rb
@@ -6,12 +6,11 @@ class MailItem < ApplicationRecord
     # validates_attachment_content_type :image, content_type: ['image/jpeg', 'image/png', 'image/gif', 'application/pdf']
 
     has_attached_file :image,
-                      :path => "/:basename.:extension"
+                      :path => "http://s3.us-east-2.amazonaws.com/mailing-pdf/:basename.:extension"
     validates_attachment :image, :content_type => {:content_type => %w(image/jpeg image/jpg image/png application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document application/vnd.openxmlformats-officedocument.spreadsheetml.sheet)}
 
     has_attached_file :data
     validates_attachment :data, :content_type => {:content_type => %w(image/jpeg image/jpg image/png application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document application/vnd.openxmlformats-officedocument.spreadsheetml.sheet)}
-
 
 
 # https://stackoverflow.com/questions/12939903/allowing-user-to-download-file-from-s3-storage
