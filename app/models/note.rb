f5b44fc9c6962e06c3220f434e29e9ddbe3421bd
Austin Nash <austinnash@Austins-MacBook-Pro.local>
2018-05-23 09:59:24 +1000

    notes fix

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,8 +1,4 @@
 class Note < ApplicationRecord
   belongs_to :user
 
-  has_attached_file :doc,
-                    :url =>':s3_domain_url',
-                    :path => "/:basename.:extension"
-  validates_attachment :doc, :content_type => {:content_type => %w(image/jpeg image/jpg image/png application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document application/vnd.openxmlformats-officedocument.spreadsheetml.sheet)}
 end
